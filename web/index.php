c249832b2b863423b525192b9098808ec3a123ad
Ubuntu <ubuntu@ip-172-31-33-239.eu-west-1.compute.internal>
2015-11-30 18:34:53 +0000

    First steps·

--- web/index.php
+++ web/index.php
@@ -60,21 +60,21 @@ function show_content(optionsId) {
 
 		<div id="home">
 			<h2>Home</h2>
-			<h3>The challenge</h3>
+			<h4>The challenge</h4>
 			
-			<p>Every sales team seeks to identify and exploit opportunities to increase sales. In addition to acquiring new customers, another way to increase sales is to target existing customers by providing them with relevant offers that might persuade them to purchase additional items. In order to be able to do that, a sales team would need to be able to build up offers that customers truly value and to identify those customers that are more likely to purchase new items.</p>
+			<p>Introduction on the challenge.</p>
 			
 			<ul style="list-style-type:circle">
-  				<li> The first objective can be achieved by developing a recommendation engine which, based on a customer's intended purchases can recommend additional items that are related to those already in the shopping basket. </li>
-  				<li> The second objective can be achieved by analyzing the average contribution that existing customers make across the entire product line. This is based on the belief that customers that have historically generated the highest revenues are not necessarily the same ones with the highest marginal revenue potential.</li>
+  				<li> The first objective can be explained here. </li>
+  				<li> Here we explain some interesting stuff.</li>
 			</ul>
 			
 			<h3>The solution</h3>
 						
-			<p>We have addressed the above challenges in two steps:</p>
+			<p>We explain our work:</p>
 				<ul style="list-style-type:circle">
-					<li> First we have implemented a simple recommendation system, based on the <b><a href="http://www.wikipedia.org/wiki/Apriori_algorithm" target="_blank">Apriori algorithm.</a></b>. This algorithm analyses existing transaction recorded in the database and develops associative rules between products, based on the frequency of them being purchased together. The output of the algorithm is a set of rules which link a set of two or more products (which are assumed to be already in the customer's basket) with an additional product that the customer is recommended to buy.</li>
-					<li> To address the second part of the challenge, i.e. to identify the company's "most interesting revenue generating customers", we have carried out a LASSO Regression, on the subset of the 20 top clients measured by total revenue, regressing each customers purchases (measure in quantities) on the total revenues associated to each product. We have used the results of this regression to rank customers according to their <b>percentage monetary contribution</b> to total revenues from buying one additional product. We have used a <b>LASSO approach</b> to identify customers for personalised marketing campaigns, allowing the company to move away from a massive and unrestricted campaign to a more efficient use of marketing resources.</li>
+					<li> Here we can also explain our recommendation system, check code for linking <b><a href="http://www.wikipedia.org/wiki/Apriori_algorithm" target="_blank">Apriori algorithm.</a></b>, maybe we can link it to the page where our recommendation system is explained .</li>
+					<li> code to make bold words here <b>seems like doing latex</b>.</li>
 				</ul>
 						
 		</div>	
@@ -83,7 +83,7 @@ function show_content(optionsId) {
 	
 	</div>
 
-	<div id="footer">Project team: Gaston Besanson, Stefano Costantini, Laura Cozma, Jordi Zamora Munt</div>
+	<div id="footer">Project team: Felix Gutmann, Marco Fayet, Max van Esso</div>
 
 </body>
 </html>
