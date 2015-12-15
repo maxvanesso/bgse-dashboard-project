3b8b9ddc91dbe5b4205e613a73ef37814d50b95e
maxvanesso <max.vanesso@barcelonagse.eu>
2015-12-15 13:35:49 +0100

    test

--- setup.R
+++ setup.R
@@ -1,2 +1,2 @@
-install.packages("recommenderlab", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
-install.packages("glmnet", repos="http://cran.r-project.org", lib="/home/ubuntu/projects/Rlibs/")
+install.packages("recommenderlab", repos="http://cran.r-project.org", lib="/home/ubuntu/bgse-dashboard-project/Rlibs/")
+install.packages("glmnet", repos="http://cran.r-project.org", lib="/home/ubuntu/bgse-dashboard-project/Rlibs/")
