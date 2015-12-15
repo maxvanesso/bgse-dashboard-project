36bdc950a1cd6a153e9eb082b886998088ea65e3
ikebgse <marcantoine.fayet@barcelonagse.eu>
2015-12-15 03:43:18 -0800

    testing recom png

--- web/index.php
+++ web/index.php
@@ -62,14 +62,14 @@ function show_content(optionsId) {
 			<h2>Home</h2>
 			<h3>The Challenge</h3>
 			
-			<p>JR Cigars originally started as a small cigar shop in Manhattan. Today, the company is one of the leading US wholesalers and retailers of cigars and tobacco related products with brick and mortar stores in North Carolina, New Jersey, DC and Michigan. They offer a wide selection of hand-rolled cigars from over 500 brands, ranging from 5 cents to $50.00 apiece.</p>
+			<p>JR Cigars originally started as a small cigar shop in Manhattan. Today, the company is one of the leading US wholesalers and retailers of cigars and tobacco related products, with brick and mortar stores in North Carolina, New Jersey, D.C. and Michigan. They offer a wide selection of hand-rolled cigars from over 500 brands, ranging from 5 cents to $50.00 apiece.</p>
 
-			<p>From a data science perspective, JR Cigars is interesting primarily because of its online and catalog sales operations from which originate the bulk of its income stream. We were able to get ahold of 2 years worth of data containing:</p>
+			<p>From a data science perspective, JR Cigars is interesting primarily because of its online and catalogue sales operations from which originate the bulk of its income stream. We were able to get a hold of 2 years worth of data containing:</p>
 			
 			<ul style="list-style-type:circle">
-  				<li> Sales information (invoice number, date of purchase, volume, cost) </li>
-  				<li> Client information (ID number, state of residence, age)</li>
-  				<li> Product information (brand, manufacturer, product category)</li>
+  				<li> Sales information : invoice data, date of purchase, volume of sales, cost of products </li>
+  				<li> Client information : ID number, state of residence, age </li>
+  				<li> Product information : product brand, manufacturer, product category</li>
 			</ul>
 			
 			<p>The original dataset spans about a million rows for a total of 15 million entries. The challenge for us was to leverage this dataset into useful business intelligence, that could for example be leveraged by the company's managers to increase profitability.</p>
@@ -78,8 +78,8 @@ function show_content(optionsId) {
 						
 			<p>We decided to adopt a two pronged approach in terms of using and treating the data.</p>
 				<ul style="list-style-type:circle">
-					<li> First, we sought to provide insights as to what was actually going on in the dataset. Displaying the right kind of information is at the heart of business intelligence. This is the descriptive part which is covered in the Data Section of our dashboard.</li>
-					<li> Second, we decided to produce inferential statistics in order to boost future sales and revenue. The two products we designed are a recommender system, based on the purchase history of clients, and a time series analysis used to predict sales. These results can be found in the Analysis Section of our Dashboard</li>
+					<li> First, we sought to provide insights as to what was actually going on in the dataset. What are the most sold brands? Where are customers located? Displaying the right kind of information is at the heart of business intelligence. This is the descriptive part which is covered in the Data Section of our dashboard.</li>
+					<li> Second, we decided to produce inferential statistics in an attempt to boost future sales and revenue. The two products we designed are a recommender system, based on the purchase history of clients, and a time series analysis used to predict sales. These results can be found in the Analysis Section of our Dashboard.</li>
 				</ul>
 						
 		</div>	
@@ -87,6 +87,8 @@ function show_content(optionsId) {
                 <?php include 'data_and_analysis.php' ?>
 	
 	</div>
+
+	<div>	<center><img src="recomsystem.png" style="width: 80%"></center></div>
 
 	<div id="footer">Project team: Felix Gutmann, Marco Fayet, Max van Esso</div>
 
