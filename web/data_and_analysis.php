b71ae55333103e3891cd989f84be1d8b51026971
FelixGSE <felix.gutmann@barcelonagse.eu>
2015-12-16 23:43:59 +0100

    ga

--- web/data_and_analysis.php
+++ web/data_and_analysis.php
@@ -101,6 +101,14 @@
 	query_and_print_series($query,$title,"Dollars");
 ?>
 
+<?php
+	// Page body. Write here your queries
+	
+	$query = "Select time,lasso, ridge from cigar.predictions";
+	$title = "Time series";
+	query_and_print_series($query,$title,"Dollars");
+?>
+
 
 	<p>Below we show the top 20 product recommendation rules identified by the <b>Apriori algorithm</b>. The table can be read as follows: for each rule, the left-hand side shows a potential basket that the customer has put together, while the right-hand side shows the additional product that could be purchased to "complete that basket".</p>
 
