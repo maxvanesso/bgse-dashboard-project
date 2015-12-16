c948fd8f4367dd1bf557e75aff02d747fccbe8be
FelixGSE <felix.gutmann@barcelonagse.eu>
2015-12-16 22:22:09 +0100

    ga

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -128,7 +128,7 @@ dbSendQuery(data.base,"DROP TABLE IF EXISTS recommendation")
 dbWriteTable(conn = data.base, name="recommendation", value=top, row.names=FALSE)
 
 
-q <- "SELECT truncate(i.Sales,2) as Sales from cigar.invoice I inner join cigar.invoice_detail i on I.InvoiceNumber=i.InvoiceNumber group by I.InvoiceDate"
+q <- "SELECT truncate(sum(i.Sales),2) as Sales from cigar.invoice I inner join cigar.invoice_detail i on I.InvoiceNumber=i.InvoiceNumber group by I.InvoiceDate"
 res <- query(data.base,q,-1)
 n <- 1:nrow(res)
 o <- data.frame(cbind(n,res))
@@ -137,5 +137,5 @@ dbSendQuery(data.base,"DROP TABLE IF EXISTS sales")
 dbWriteTable(conn = data.base, name="sales", value=o, row.names=FALSE)
 
 
-
+plot(res$Sales)
 
